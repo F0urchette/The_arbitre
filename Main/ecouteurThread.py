6d3dac715449981dbcabdb9d9dfda30b8cc80e36
Antoine <antoine.ravel@univ-tlse3.fr>
2018-05-21 14:14:39 +0200

    Réglages.

--- Main/ecouteurThread.py
+++ Main/ecouteurThread.py
@@ -23,12 +23,7 @@ class ClientThread(threading.Thread):
 
     def run(self):
         print("Connection de %s %s" % (self.ip, self.port,))
-        res = ""
-        print ("res : ",res)
         received_chunks = []
-        buf_size = 5448429
-        size = buf_size
-        remaining = size
         while True:
             received = self.clientsocket.recv(1024)
             if not received:
@@ -36,40 +31,19 @@ class ClientThread(threading.Thread):
             received_chunks.append(received)
             #remaining -= len(received)
         res = (b''.join(received_chunks))
-        print (res)
+        #print (res)
         now = datetime.datetime.now()
         filename = str(('%s_h_%s_m_%s_s'%(now.hour,now.minute,now.second)))+".jpeg"
-        self.numero = self.numero+1
-        #print ("self.numero : "),self.numero
-        #base64.b64decode(bytes(res, 'utf-8'))
-        imgdata = decode_base64(res)
+        imgdata = ""
+        #res += "=" * ((4 - len(res) % 4) % 4)
+        outputdata = decode_base64(res)
+        print(outputdata)
         with open(filename, 'wb') as f:
-            f.write(imgdata)
+            f.write(outputdata)
 
-
-
-def recvall2(s, size):
-    received_chunks = []
-    buf_size = 4096
-    remaining = size
-    while remaining > 0:
-        received = s.recv(min(remaining, buf_size))
-        if not received:
-            raise Exception('unexcepted EOF')
-        received_chunks.append(received)
-        remaining -= len(received)
-    return b''.join(received_chunks)
-
-#TODO : METTRRE LE I HORS DE LA FONCTION;
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 tcpsock.bind(("", 5001))
-
-def readb64(base64_string):
-    sbuf = BytesIO()
-    sbuf.write(base64.b64decode(base64_string))
-    pimg = Image.open(sbuf)
-    return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
 def decode_base64(data):
     """Decode base64, padding being optional.
@@ -88,4 +62,5 @@ while True:
     print("En écoute...")
     (clientsocket, (ip, port)) = tcpsock.accept()
     newthread = ClientThread(ip, port, clientsocket)
+    #newthread.daemon = True
     newthread.start()
